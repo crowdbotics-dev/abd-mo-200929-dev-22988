5c381f2e06a284b9420faf3dd7045319ca8c9355
Crowdbotics <engineering@crowdbotics.com>
2021-09-29 13:49:46 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,15 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "abd_mo_200929_dev_22988",
   url: "https://abd_mo_200929_dev_22988.botics.co",
   api: "https://abd_mo_200929_dev_22988.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    title: "App Menu new",
+    copy: "Routes available! new",
+    styles: styles
   }
 }
